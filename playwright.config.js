568ea1cb4b42239f09a0585c4411a5e1ba1790ab
X4\/¡ G <xavier.gonzalez.arriola.liza@gmail.com>
2026-02-24 22:11:36 +0100

    Add Galaxy phone/tablet Playwright projects (galaxy-s21, galaxy-tab-s7)

--- playwright.config.js
+++ playwright.config.js
@@ -18,6 +18,30 @@ module.exports = defineConfig({
     { name: 'pixel5', use: { ...devices['Pixel 5'] } },
     { name: 'iphone13', use: { ...devices['iPhone 13'] } },
     { name: 'ipad-gen7', use: { ...devices['iPad (gen 7)'] } }
+    ,
+    // Samsung Galaxy phone and tablet examples
+    {
+      name: 'galaxy-s21',
+      use: {
+        // start from Pixel 5 descriptor then override to represent a Samsung Galaxy phone
+        ...devices['Pixel 5'],
+        userAgent:
+          'Mozilla/5.0 (Linux; Android 11; SM-G991B) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.120 Mobile Safari/537.36',
+        viewport: { width: 412, height: 915 }
+      }
+    },
+    {
+      name: 'galaxy-tab-s7',
+      use: {
+        // Tablet-like viewport and touch support
+        viewport: { width: 1200, height: 2000 },
+        deviceScaleFactor: 2,
+        isMobile: true,
+        hasTouch: true,
+        userAgent:
+          'Mozilla/5.0 (Linux; Android 11; SM-T870) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.120 Safari/537.36'
+      }
+    }
   ],
 
   use: {
